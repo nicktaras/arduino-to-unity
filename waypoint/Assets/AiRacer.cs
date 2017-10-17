be31a94f1d9028e7168a25937f3f2e95ff9aa6a7
Nicholas Taras <nicholas.taras@nab.com.au>
2017-10-18 08:56:21 +1100

    added waypoint demo

---
+++ waypoint/Assets/AiRacer.cs
@@ -0,0 +1,85 @@
+﻿/* AiRacer (UNITY C#)
+* 
+* AiRacer:
+* 
+* USE:
+* - To orchestrate the racer ai.
+* - This class has waypoints and move towards classes.
+* 
+* PARAMS:
+* - tagName of way point to use
+* 
+* TEST: 
+* tbc
+* 
+* AUTHOR: 
+* Nicholas Taras
+*
+*/
+
+using UnityEngine;
+using System.Collections;
+
+public class AiRacer : MonoBehaviour
+{
+
+	public class Racer
+	{
+		public GameObject wayPointTarget;
+		public bool isAllowedToRace;
+				
+		public Racer (bool _isAllowedToRace)
+		{
+			isAllowedToRace = _isAllowedToRace;
+		}
+
+		public Racer ()
+		{
+			isAllowedToRace = false;
+		}
+	}
+		
+	public Racer racer = new Racer(false);
+
+	void Start()
+	{
+		racer.isAllowedToRace = true;
+	}
+		
+	bool wayPointReached(GameObject _target){
+
+		Vector3 currentPosition = transform.position;
+
+		bool _wayPointReached = false;
+
+		float distance = (currentPosition - _target.transform.position).sqrMagnitude;
+
+		if (distance < Random.Range(1, 2)) { // TODO UPDATE AND RANDOMISE VALUE HERE.
+			_wayPointReached = true;
+		}
+
+		return _wayPointReached;
+
+	}
+
+	void race () {
+
+		AiWayPointsSequential _aiWayPointsSequential = GetComponent<AiWayPointsSequential>();
+		racer.wayPointTarget = _aiWayPointsSequential.getCurrentWayPoint ();
+	
+		MoveTowards _moveTowards = GetComponent<MoveTowards>();
+		_moveTowards.moveToTarget (racer.wayPointTarget);
+
+	}
+
+	void Update () {
+
+		if (racer.isAllowedToRace) {
+
+			race ();
+				
+		}
+			
+	}
+
+}
---
+++ waypoint/README.md
@@ -0,0 +1,11 @@
+# Unity 3-D C# racing_poc_waypoints
+A proof of concept app to animate 3d objects to waypoints. 
+
+### This project can be used to help you get a head start with a Unity racing game. Or a simulation where you must send objects to specific locations (waypoints). 
+
+Code within this poc includes: way finding, directional rotation towards a target.
+
+## To run:
+
+Download a copy of Unity 3D version 2017.1+
+Press the play button to execute the application in development mode.
